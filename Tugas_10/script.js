f72ed594b19bed9f725ecf388ef458246ed62e4e
Steephen .P.S <steephensitumeang93@gmail.com>
2025-06-24 11:36:21 +0700

    Update Tugas 11 dan 12

--- Tugas_10/script.js
+++ Tugas_10/script.js
@@ -1,14 +1,11 @@
-// Event listener untuk tombol Register
 document.getElementById("registerBtn").addEventListener("click", function() {
     alert("Website Dalam Perbaikan");
 });
 
-// Event listener untuk tombol Login
 document.getElementById("loginBtn").addEventListener("click", function() {
     alert("Website Dalam Perbaikan");
 });
 
-// Animasi perubahan genre
 const genres = ["HORROR", "ACTION", "ROMANCE", "COMEDY"];
 let index = 0;
 
